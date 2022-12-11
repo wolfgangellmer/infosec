71b8a849afd9a7e945596ccf8bf7e4a761dc8af1
WolfgangEllmer <wolfgang.ellmer@live.at>
2022-12-12 00:34:13 +0100

    inst

---
+++ infra/windows/checks/inf-win-chck-windowsupdates.ps1
@@ -0,0 +1,14 @@
+#updates search
+UsoClient StartScan
+
+#download updates
+UsoClient StartDownload
+
+#install updates
+UsoClient StartInstall
+
+#restart after updates
+UsoClient RestartDevice
+
+#updates scan + download + install
+UsoClient ScanInstallWait 
